76733cc10e12dbae9b0cbabfe2095f1d3935b7ed
Atanu Bhowmick <51965855+atanubhowmick@users.noreply.github.com>
2020-02-10 13:14:42 +0530

    Error message updated

--- src/main/java/com/atanu/spring/product/constant/ErrorCode.java
+++ src/main/java/com/atanu/spring/product/constant/ErrorCode.java
@@ -11,7 +11,7 @@ public enum ErrorCode {
 
 	PE001("No Product found"), 
 	PE002("No Search result found"),
-	PE003("Invalid Request. Page cannot be less than 1."),
+	PE003("Invalid Request. Page cannot be less than 0."),
 	PE004("Invalid Request. Size cannot be less than 1."),
 	PE005("Invalid Request. Some fields are missing.");
 
